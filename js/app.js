f089d5a5b44378296c5f333d909df974cf7b3009
Karthi UIUX <50635106+kar-thi@users.noreply.github.com>
2020-05-04 13:51:20 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -18,3 +18,80 @@ $(window).scroll(function() {
   });
 
   
+
+
+
+
+  $(function(){
+
+    window.sr = ScrollReveal();
+  
+    if ($(window).width() < 768) {
+  
+      if ($('.timeline-content').hasClass('js--fadeInLeft')) {
+        $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+      }
+  
+      sr.reveal('.js--fadeInRight', {
+        origin: 'right',
+        distance: '30px',
+        easing: 'ease-in-out',
+        duration: 800,
+      });
+  
+    } else {
+      
+      sr.reveal('.js--fadeInLeft', {
+        origin: 'left',
+        distance: '30px',
+        easing: 'ease-in-out',
+        duration: 800,
+      });
+  
+      sr.reveal('.js--fadeInRight', {
+        origin: 'right',
+        distance: '30px',
+        easing: 'ease-in-out',
+        duration: 800,
+      });
+  
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+        origin: 'left',
+        distance: '300px',
+        easing: 'ease-in-out',
+        duration: 800,
+      });
+  
+      sr.reveal('.js--fadeInRight', {
+        origin: 'right',
+        distance: '30px',
+        easing: 'ease-in-out',
+        duration: 800,
+      });
+  
+  
+  });
+
+
+
+
+
+  (function () {
+
+    // Setting up the options
+    var options = {
+        startLength: 0, // start positioning the slider items at the beginning of the SVG path
+        duration: 3000, // animation duration (used by anime.js)
+        stagger: 15, // incrementally delays among items, producing an staggering effect
+        easing: 'easeOutElastic', // easing function (used by anime.js)
+        elasticity: 600, // elasticity factor (used by anime.js)
+        rotate: true // This indicates that items should be rotated properly to match the SVG path curve
+    };
+
+    // Initialize the slider using our SVG path, items, and options
+    new PathSlider('.path-slider__path', '.path-slider__item', options);
+
+})();
+
